a309fb3693e2963bb5c4c6b2102c988914c5b2b7
Дмитрий Ярмоленко <yarddim@gmail.com>
2017-08-03 21:59:34 +0300

    Game tiles update

--- TicTacToe/Assets/Scripts/LevelManager.cs
+++ TicTacToe/Assets/Scripts/LevelManager.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(transform.gameObject);
-    }
-
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
