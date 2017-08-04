3e9cc7be8c3fe3363cc9cd19b6ac6cadccd3335a
Дмитрий Ярмоленко <yarddim@gmail.com>
2017-08-04 12:10:40 +0300

    Two players version

---
+++ TicTacToe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum State { Idle, XTurn, OTurn, XWin, OWin, Draw };
+
+    private int[] tiles = new int[9];
+    private int lastAvailable = 9;
+    private bool playerTurn = true;
+    private int[,] winners = {
+        {0, 1, 2 },
+        {3, 4, 5 },
+        {6, 7, 8 },
+        {0, 3, 6 },
+        {1, 4, 7 },
+        {2, 5, 8 },
+        {0, 4, 8 },
+        {2, 4, 6 }
+    };
+
+    private State currentState;
+    
+    private void SetState(State newState)
+    {
+        currentState = newState;
+    }
+    // Use this for initialization
+	void Start () {
+        SetState(State.Idle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(CheckWinner());
+
+        switch (currentState)
+        {
+            case State.Idle:
+
+                Debug.Log("Idle State");
+                SetState(State.XTurn);
+                break;
+
+            case State.XTurn:
+                Debug.Log("XTurn State");
+
+                if (CheckWinner())
+                {
+                    SetState(State.XWin);
+                }
+                else if (lastAvailable - CountAvailable() == 1)
+                {
+                    lastAvailable--;
+
+                    if (CountAvailable() == 0)
+                    {
+                        SetState(State.Draw);
+                    }
+                    else
+                    {
+                        SetState(State.OTurn);
+                    }
+                }
+                
+
+                break;
+            case State.OTurn:
+                Debug.Log("OTurn State");
+
+                if (CheckWinner())
+                {
+                    SetState(State.OWin);
+                }
+                else if (lastAvailable - CountAvailable() == 1)
+                {
+                    lastAvailable--;
+
+                    if (CountAvailable() == 0)
+                    {
+                        SetState(State.Draw);
+                    }
+                    else
+                    {
+                        SetState(State.XTurn);
+                    }
+                }
+                break;
+            case State.XWin:
+
+                Debug.Log("XWin state");
+                break;
+            case State.OWin:
+
+                Debug.Log("OWin state");
+                break;
+            case State.Draw:
+
+                Debug.Log("Draw State");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetTile (int index)
+    {
+        tiles[index] = State.XTurn == currentState ? 1 : 2;
+    }
+
+    private bool CheckWinner()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (tiles[winners[i,0]] == tiles[winners[i,1]] && tiles[winners[i,1]] == tiles[winners[i,2]])
+            {
+                if (tiles[winners[i,0]]!= 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private int CountAvailable()
+    {
+        int availableTiles = 0;
+
+        foreach (int tile in tiles)
+        {
+            if (tile == 0)
+            {
+                availableTiles++;
+            }
+        }
+
+        return availableTiles;
+    }
+
+    public State GetState()
+    {
+        return currentState;
+    }
+}
--- TicTacToe/Assets/Scripts/Tile.cs
+++ TicTacToe/Assets/Scripts/Tile.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Tile : MonoBehaviour {
 
     public int id;
+    public Sprite cross, zero;
+
+    private GameManager gameManager;
+    private SpriteRenderer spriteRenderer;
+    private bool available = true;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void OnMouseDown()
     {
-        Debug.Log(name + "clicked");
+        if (available)
+        {
+            spriteRenderer.sprite = gameManager.GetState() == GameManager.State.XTurn ? cross : zero;
+            gameManager.SetTile(id);
+            available = false;
+        } 
     }
 }
